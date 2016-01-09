662893145f8b7ee2ccdf71534cda1d27bec749d4
fhc02hpg <fhc02hpg@users.noreply.github.com>
2016-01-09 20:18:38 +0100

    LV Code einfache mathem. Rekursion

---
+++ src/org/campus02/pr1/recursion/lv/DemoRecursions.java
@@ -0,0 +1,15 @@
+package org.campus02.pr1.recursion.lv;
+
+public class DemoRecursions {
+
+	public static void main(String[] args) {
+		
+		int result = RecursiveMaths.sum(10);
+		System.out.println(result);
+		
+		result = RecursiveMaths.fibonacci(8);
+		System.out.println(result);
+		
+	}
+
+}
---
+++ src/org/campus02/pr1/recursion/lv/RecursiveMaths.java
@@ -0,0 +1,19 @@
+package org.campus02.pr1.recursion.lv;
+
+public class RecursiveMaths {
+
+	public static int sum(int n) {
+		if(n == 0) {
+			return 0;
+		}
+		return sum(n-1)+n;
+	}
+	
+	public static int fibonacci(int n) {
+		if(n <= 2) {
+			return 1;
+		}
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
+	
+}
